8beda78df3241cb2ed1caa60ae5b9024c4209b75
sankar9035 <sankarrudrapal@gmail.com>
2024-03-31 17:08:39 +0530

    data type concept

--- lab_data.py
+++ lab_data.py
@@ -1,6 +1,11 @@
 
 # python Data_type
 # Numeric data type: Int, Float, Complex.
+# Boolean type : bool
+# Sequence Type : String, List and Tuple.
+# Mapping type : Dict
+# Set
+# None
 a = 10
 b = 3.587
 c = 10+20j
