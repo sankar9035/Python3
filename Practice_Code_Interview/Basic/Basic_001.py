36d18d18ef12627f6aacd88c5cea3c840e2a6f10
sankar9035 <sankarrudrapal@gmail.com>
2026-04-10 14:17:55 +0530

    interview code add

---
+++ Practice_Code_Interview/Basic/Basic_001.py
@@ -0,0 +1,15 @@
+#Write a Python code to check if a number is even or odd
+
+# Function to check if a number is even or odd
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is Even"
+    else:
+        return f"{number} is Odd"
+
+# Input from the user
+try:
+    user_input = int(input("Enter a number: "))
+    print(check_even_odd(user_input))
+except ValueError:
+    print("Invalid input! Please enter an integer.")
