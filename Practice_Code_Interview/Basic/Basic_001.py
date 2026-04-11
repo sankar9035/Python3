21725546ccd54a5c21e8f3ce60ac5fe75eceb9ad
sankar9035 <sankarrudrapal@gmail.com>
2026-04-11 10:31:42 +0530

    Interview question added

--- Practice_Code_Interview/Basic/Basic_001.py
+++ Practice_Code_Interview/Basic/Basic_001.py
@@ -13,3 +13,16 @@ try:
     print(check_even_odd(user_input))
 except ValueError:
     print("Invalid input! Please enter an integer.")
+
+print("--- Test with some examples ---")
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is Even"
+    else:
+        return f"{number} is Odd"
+
+# Test with some examples
+numbers = [0, 1, 7, 12, 25, 100, -3, -8]
+for num in numbers:
+    print(check_even_odd(num))
+
